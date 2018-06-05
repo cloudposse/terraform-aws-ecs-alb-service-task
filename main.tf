9e0e19b0704e4b11b9ae78067ebb50c8d1205d12
Sarkis Varozian <svarozian@gmail.com>
2018-06-05 15:55:02 -0700

    move out policy jsons to data document

--- main.tf
+++ main.tf
@@ -113,21 +113,46 @@ data "aws_iam_policy_document" "ecs_service_policy" {
 
 resource "aws_iam_role_policy" "ecs_service_role_policy" {
   name = "ecs_service_role_policy"
-
-  #policy = "${file("${path.module}/policies/ecs-service-role.json")}"
   policy = "${data.aws_iam_policy_document.ecs_service_policy.json}"
   role   = "${aws_iam_role.ecs_role.id}"
 }
 
 /* role that the Amazon ECS container agent and the Docker daemon can assume */
+data "aws_iam_policy_document" "ecs_task_execution_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ecs_execution_role" {
   name               = "ecs_task_execution_role"
-  assume_role_policy = "${file("${path.module}/policies/ecs-task-execution-role.json")}"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_task_execution_role.json}"
+}
+
+data "aws_iam_policy_document" "ecs_execution_role" {
+  statement {
+    effect = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ],
+  }
 }
 
 resource "aws_iam_role_policy" "ecs_execution_role_policy" {
   name   = "ecs_execution_role_policy"
-  policy = "${file("${path.module}/policies/ecs-execution-role-policy.json")}"
+  policy = "${data.aws_iam_policy_document.ecs_execution_role.json}"
   role   = "${aws_iam_role.ecs_execution_role.id}"
 }
 
