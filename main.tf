2d8fbcc4c89875b782871fc995a0ea6eacbb576a
Sarkis Varozian <svarozian@gmail.com>
2018-06-05 08:19:27 -0700

    checkpoint

--- main.tf
+++ main.tf
@@ -1,2 +1,183 @@
+# TODO: (input) Create VPC outside of this module
+# TODO: (input) Create Subnets (Private / Public) outside of this module
+# TODO: (input) Create ACM cert outside of this module
 
+# TODO: use `terraform-terraform-label` to generate all labels.
+
+# TODO: (output) security group IDs
+
+#module "container_definition" {
+#  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-container-definition.git?ref=init"
+#  container_name      = "app"
+#  container_image    = "nginx"
+#}
+
+
+# ECR repository
+resource "aws_ecr_repository" "app" {
+  name = "${var.app_name}"
+}
+
+
+# ECS Task def
+data "template_file" "web_task" {
+  template = "${file("task_definition.json")}"
+
+  vars {
+    image           = "${aws_ecr_repository.app.repository_url}"
+    #log_group       = "${aws_cloudwatch_log_group.app.name}"
+  }
+}
+
+resource "aws_ecs_task_definition" "web" {
+  family                   = "${var.stage}_web"
+  container_definitions    = "${data.template_file.web_task.rendered}"
+  #container_definitions = "${module.container_definition.container_definitions}"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "${var.task_cpu}"
+  memory                   = "${var.task_memory}"
+  execution_role_arn       = "${aws_iam_role.ecs_execution_role.arn}"
+  task_role_arn            = "${aws_iam_role.ecs_execution_role.arn}"
+}
+
+# ALB
+
+## Target Group
+resource "random_id" "target_group_sufix" {
+  byte_length = 2
+}
+
+resource "aws_alb_target_group" "alb_target_group" {
+  name     = "${var.stage}-alb-target-group-${random_id.target_group_sufix.hex}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+  target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## Listener
+resource "aws_alb_listener" "app" {
+  load_balancer_arn = "${var.alb_arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  depends_on        = ["aws_alb_target_group.alb_target_group"]
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.alb_target_group.arn}"
+    type             = "forward"
+  }
+}
+
+# IAM
+data "aws_iam_policy_document" "ecs_service_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_role" {
+  name               = "ecs_role"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_service_role.json}"
+}
+
+data "aws_iam_policy_document" "ecs_service_policy" {
+  statement {
+    effect = "Allow"
+    resources = ["*"]
+    actions = [
+      "elasticloadbalancing:Describe*",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "ec2:Describe*",
+      "ec2:AuthorizeSecurityGroupIngress"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_service_role_policy" {
+  name   = "ecs_service_role_policy"
+  #policy = "${file("${path.module}/policies/ecs-service-role.json")}"
+  policy = "${data.aws_iam_policy_document.ecs_service_policy.json}"
+  role   = "${aws_iam_role.ecs_role.id}"
+}
+
+/* role that the Amazon ECS container agent and the Docker daemon can assume */
+resource "aws_iam_role" "ecs_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = "${file("${path.module}/policies/ecs-task-execution-role.json")}"
+}
+resource "aws_iam_role_policy" "ecs_execution_role_policy" {
+  name   = "ecs_execution_role_policy"
+  policy = "${file("${path.module}/policies/ecs-execution-role-policy.json")}"
+  role   = "${aws_iam_role.ecs_execution_role.id}"
+}
+
+
+# Service
+##  ECS cluster
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.stage}-ecs-cluster"
+}
+
+## Security Groups
+resource "aws_security_group" "ecs_service" {
+  vpc_id      = "${var.vpc_id}"
+  name        = "${var.stage}-ecs-service-sg"
+  description = "Allow egress from container"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name        = "${var.stage}-ecs-service-sg"
+    Stage = "${var.stage}"
+  }
+}
+
+data "aws_ecs_task_definition" "web" {
+  task_definition = "${aws_ecs_task_definition.web.family}"
+}
+
+resource "aws_ecs_service" "web" {
+  name            = "${var.stage}-web"
+  task_definition = "${aws_ecs_task_definition.web.family}:${max("${aws_ecs_task_definition.web.revision}", "${data.aws_ecs_task_definition.web.revision}")}"
+  desired_count   = "${var.desired_count}"
+  launch_type     = "FARGATE"
+  cluster =       "${aws_ecs_cluster.cluster.id}"
+  depends_on      = ["aws_iam_role_policy.ecs_service_role_policy"]
+
+  network_configuration {
+    security_groups = ["${aws_security_group.ecs_service.id}"]
+    subnets         = ["${var.private_subnet_ids}"]
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.alb_target_group.arn}"
+    container_name   = "web"
+    container_port   = "80"
+  }
+
+  depends_on = ["aws_alb_target_group.alb_target_group"]
+}
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,50 @@
-variable "alb_id" {}
+variable "app_name" {
+  description = "The name of the app."
+}
 
-variable "container_defintions" {}
+variable "vpc_id" {
+  description = "The VPC id where resources are created."
+}
 
+variable "alb_arn" {
+  description = "The ALB arn where listener and target group will be created."
+}
 
+variable "stage" {
+  description = "Stage of the resources."
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet ids."
+}
+
+variable "public_subnet_ids" {
+  description = "Private subnet ids."
+}
+
+#variable "acm_arn" {
+#  description = "The arn of the ACM certificate."
+#}
+
+variable "desired_count" {
+  description = "The number of instances of the task definition to place and keep running."
+  default = 1
+}
+
+variable "service_name" {
+  description = "The name of the service (up to 255 letters, numbers, hyphens and underscores)."
+}
+
+variable "launch_type" {
+  description = "The launch type on which to run your service. Valid values are EC2 and FARGATE."
+}
+
+variable "task_cpu" {
+  description = "The number of CPU units used by the task. If using Fargate launch type task_cpu must match supported memory values (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)."
+  default = 256
+}
+
+variable "task_memory" {
+  description = "The amount of memory (in MiB) used by the task. If using Fargate launch type task_memory must match supported cpu value (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)."
+  default = 512
+}
