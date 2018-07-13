7ad1f837609d7eb6f48b1bcbb6708a865821b233
Sarkis Varozian <svarozian@gmail.com>
2018-07-13 16:59:07 -0700

    fix output

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,10 @@ output "service_name" {
 
 output "service_role_arn" {
   description = "ECS Service role ARN"
-  value       = "${aws_iam_role.ecs_service_role.arn}"
+  value       = "${aws_iam_role.ecs_service.arn}"
 }
 
 output "task_role_arn" {
   description = "ECS Task role ARN"
-  value       = "${aws_iam_role.ecs_task_role.arn}"
+  value       = "${aws_iam_role.ecs_task.arn}"
 }
