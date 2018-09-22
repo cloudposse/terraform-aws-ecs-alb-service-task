2b0711003e77a5c922a18f7256fc77ef33a67078
rryke <ryan@itsacloudlife.com>
2018-09-22 13:03:24 -0700

    output ecs security group
    want to use this in other modules

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "task_role_arn" {
   description = "ECS Task role ARN"
   value       = "${aws_iam_role.ecs_task.arn}"
 }
+
+output "ecs_sg_id" {
+  description = "Security Group id of ECS task"
+  value       = "${aws_security_group.ecs_service.id}"
+}
