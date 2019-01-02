af7955f29ceac698c4564539182c7bb76052b802
Samuli Saarinen <samuli.saarinen@remion.com>
2019-01-02 10:14:51 +0200

    Make volumes configurable

--- variables.tf
+++ variables.tf
@@ -113,3 +113,9 @@ variable "health_check_grace_period_seconds" {
   description = "Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 7200. Only valid for services configured to use load balancers."
   default     = 0
 }
+
+variable "volumes" {
+  type = "list"
+  description = "Task volume definitions as list of maps"
+  default     = []
+}
