4b15d96f52829e39eb9c7a6646f6426ccb4c1042
Samuli Saarinen <samuli.saarinen@remion.com>
2019-01-15 10:54:20 +0200

    Expose aws_ecs_service health_check_grace_period_seconds

--- main.tf
+++ main.tf
@@ -181,6 +181,7 @@ resource "aws_ecs_service" "default" {
   desired_count                      = "${var.desired_count}"
   deployment_maximum_percent         = "${var.deployment_maximum_percent}"
   deployment_minimum_healthy_percent = "${var.deployment_minimum_healthy_percent}"
+  health_check_grace_period_seconds  = "${var.health_check_grace_period_seconds}"
   launch_type                        = "${var.launch_type}"
   cluster                            = "${var.ecs_cluster_arn}"
   tags                               = "${module.default_label.tags}"
