4fc3eb154b95f4691dd0375346f9a3b9f854010d
Samuli Saarinen <samuli.saarinen@remion.com>
2019-01-21 12:26:29 +0200

    Add type to health_check_grace_period_seconds

--- variables.tf
+++ variables.tf
@@ -109,6 +109,7 @@ variable "deployment_minimum_healthy_percent" {
 }
 
 variable "health_check_grace_period_seconds" {
+  type        = "string"
   description = "Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 7200. Only valid for services configured to use load balancers."
   default     = 0
 }
