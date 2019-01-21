082871d3b169503d7deb8364b1a96675269f74b4
Samuli Saarinen <samuli.saarinen@gmail.com>
2019-01-21 12:31:48 +0200

    Expose aws_ecs_service health_check_grace_period_seconds (#19)
    * Expose aws_ecs_service health_check_grace_period_seconds
    
    * Add type to health_check_grace_period_seconds

--- variables.tf
+++ variables.tf
@@ -107,3 +107,9 @@ variable "deployment_minimum_healthy_percent" {
   description = "The lower limit (as a percentage of `desired_count`) of the number of tasks that must remain running and healthy in a service during a deployment"
   default     = 100
 }
+
+variable "health_check_grace_period_seconds" {
+  type        = "string"
+  description = "Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 7200. Only valid for services configured to use load balancers."
+  default     = 0
+}
