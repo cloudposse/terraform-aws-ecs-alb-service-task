2f98719b9317a1f078bfb480c0e1ef17f24f38f6
Gaurav Rawat <gaurawat@publicisgroupe.net>
2019-07-30 00:30:07 -0400

    Format file

--- main.tf
+++ main.tf
@@ -177,7 +177,7 @@ resource "aws_security_group_rule" "allow_icmp_ingress" {
 }
 
 resource "aws_ecs_service" "ignore_changes_task_definition" {
-  count                              = "${var.ignore_changes_task_definition == "true" && var.network_mode != "awsvpc"? 1: 0}"
+  count                              = "${var.ignore_changes_task_definition == "true" && var.network_mode != "awsvpc" ? 1 : 0}"
   name                               = "${module.default_label.id}"
   task_definition                    = "${aws_ecs_task_definition.default.family}:${aws_ecs_task_definition.default.revision}"
   desired_count                      = "${var.desired_count}"
@@ -210,7 +210,7 @@ resource "aws_ecs_service" "ignore_changes_task_definition" {
 }
 
 resource "aws_ecs_service" "default" {
-  count                              = "${var.ignore_changes_task_definition == "false" && var.network_mode != "awsvpc"? 1: 0}"
+  count                              = "${var.ignore_changes_task_definition == "false" && var.network_mode != "awsvpc" ? 1 : 0}"
   name                               = "${module.default_label.id}"
   task_definition                    = "${aws_ecs_task_definition.default.family}:${aws_ecs_task_definition.default.revision}"
   desired_count                      = "${var.desired_count}"
@@ -237,9 +237,9 @@ resource "aws_ecs_service" "default" {
     container_port   = "${var.container_port}"
   }
 }
- 
+
 resource "aws_ecs_service" "ignore_changes_task_definition_awsvpc" {
-  count                              = "${var.ignore_changes_task_definition == "true" && var.network_mode == "awsvpc"? 1: 0}"
+  count                              = "${var.ignore_changes_task_definition == "true" && var.network_mode == "awsvpc" ? 1 : 0}"
   name                               = "${module.default_label.id}"
   task_definition                    = "${aws_ecs_task_definition.default.family}:${aws_ecs_task_definition.default.revision}"
   desired_count                      = "${var.desired_count}"
@@ -252,7 +252,7 @@ resource "aws_ecs_service" "ignore_changes_task_definition_awsvpc" {
 
   deployment_controller {
     type = "${var.deployment_controller_type}"
-  } 
+  }
 
   load_balancer {
     target_group_arn = "${var.alb_target_group_arn}"
@@ -266,7 +266,7 @@ resource "aws_ecs_service" "ignore_changes_task_definition_awsvpc" {
 }
 
 resource "aws_ecs_service" "default_awsvpc" {
-  count                              = "${var.ignore_changes_task_definition == "false" && var.network_mode == "awsvpc"? 1: 0}"
+  count                              = "${var.ignore_changes_task_definition == "false" && var.network_mode == "awsvpc" ? 1 : 0}"
   name                               = "${module.default_label.id}"
   task_definition                    = "${aws_ecs_task_definition.default.family}:${aws_ecs_task_definition.default.revision}"
   desired_count                      = "${var.desired_count}"
@@ -279,8 +279,8 @@ resource "aws_ecs_service" "default_awsvpc" {
 
   deployment_controller {
     type = "${var.deployment_controller_type}"
-  } 
-  
+  }
+
   load_balancer {
     target_group_arn = "${var.alb_target_group_arn}"
     container_name   = "${var.container_name}"
