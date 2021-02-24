21b5a9a0886b9430296bebdc61905d8d68025d11
nitro <nitrocode@users.noreply.github.com>
2021-02-24 18:35:14 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -315,8 +315,8 @@ variable "use_old_arn" {
   default     = false
 }
 
-variable "task_definition_arn" {
+variable "task_definition" {
   type        = bool
-  description = "Reuse an existing task definition arn instead of creating one"
+  description = "Reuse an existing task definition family and revision for the ecs service instead of creating one"
   default     = null
 }
