73a0432acb2f9ea311fa3efa88bd5228ce4f6a45
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2021-12-22 05:21:08 +0000

    Auto Format

--- main.tf
+++ main.tf
@@ -271,7 +271,7 @@ module "security_group" {
   source  = "cloudposse/security-group/aws"
   version = "0.4.2"
 
-  enabled = local.security_group_enabled
+  enabled                       = local.security_group_enabled
   security_group_name           = var.security_group_name
   create_before_destroy         = var.security_group_create_before_destroy
   security_group_create_timeout = var.security_group_create_timeout
@@ -280,7 +280,7 @@ module "security_group" {
   security_group_description = var.security_group_description
   allow_all_egress           = true
   rules                      = var.additional_security_group_rules
-  vpc_id          = var.vpc_id
+  vpc_id                     = var.vpc_id
 
   context = module.service_label.context
 }
