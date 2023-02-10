6a7bf0c0541fbd1131f9ea59b41be4663412889b
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-02-10 15:37:20 +0100

    feat: add_variable_for_ecs_service_role (#2)

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
   create_task_role        = local.enabled && length(var.task_role_arn) == 0
   task_exec_role_arn      = try(var.task_exec_role_arn[0], tostring(var.task_exec_role_arn), "")
   create_exec_role        = local.enabled && length(var.task_exec_role_arn) == 0
-  enable_ecs_service_role = module.this.enabled && var.network_mode != "awsvpc" && length(var.ecs_load_balancers) >= 1
+  enable_ecs_service_role = module.this.enabled && var.network_mode != "awsvpc" && length(var.ecs_load_balancers) >= 1 && var.ecs_service_role_enabled
   create_security_group   = local.enabled && var.network_mode == "awsvpc" && var.security_group_enabled
 
   volumes = concat(var.docker_volumes, var.efs_volumes, var.fsx_volumes, var.bind_mount_volumes)
