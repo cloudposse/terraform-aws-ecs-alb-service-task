cca01167366f022cf04c1639ce858eb3a3d2c835
Monica Hart <mhart2012@gmail.com>
2024-01-15 10:25:14 -0600

    Setting autoscaling aws_appautoscaling_policy count instances (#6)
    * adding autoscaling capabilites
    
    * removing ecs cluster name var
    
    * fixing resource instance key
    
    * fixing resource instance key
    
    * adding count attribute for role
    
    * adding count attribute

--- autoscaling.tf
+++ autoscaling.tf
@@ -40,9 +40,9 @@ resource "aws_appautoscaling_policy" "ecs_target_cpu" {
   count              = var.autoscaling_enabled ? 1 : 0
   name               = "application-scaling-policy-cpu"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ecs_target[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target[count.index].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target[count.index].service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
@@ -57,9 +57,9 @@ resource "aws_appautoscaling_policy" "ecs_target_memory" {
   count              = var.autoscaling_enabled ? 1 : 0
   name               = "application-scaling-policy-memory"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ecs_target[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target[count.index].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target[count.index].service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
