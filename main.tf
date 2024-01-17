8750ee095dbfbb013e3743a3abb897fa23e819c4
Monica Hart <mhart2012@gmail.com>
2024-01-17 13:45:59 -0700

    Updating family to use ecs_service_name var (#8)
    * adding autoscaling capabilites
    
    * removing ecs cluster name var
    
    * fixing resource instance key
    
    * fixing resource instance key
    
    * adding count attribute for role
    
    * adding count attribute
    
    * removing role arn, not needed
    
    * changing back to name
    
    * updating to use ecs_service_name instead of mod name

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ module "exec_label" {
 
 resource "aws_ecs_task_definition" "default" {
   count                    = local.create_task_definition ? 1 : 0
-  family                   = module.this.name
+  family                   = var.ecs_service_name
   container_definitions    = var.container_definition_json
   requires_compatibilities = [var.launch_type]
   network_mode             = var.network_mode
