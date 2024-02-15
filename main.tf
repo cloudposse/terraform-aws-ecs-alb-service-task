974857fce177fc5b3d2d48e5a5d907b194b6f012
Edson Campos <edson.campos@swile.com.br>
2024-02-15 12:03:48 -0300

    fix: type inconsistence

--- main.tf
+++ main.tf
@@ -735,7 +735,7 @@ resource "aws_ecs_service" "ignore_changes_task_definition_and_desired_count" {
 resource "aws_ecs_service" "ignore_changes_task_definition_and_desired_count_and_load_balancer" {
   count                              = local.ecs_service_enabled && var.ignore_changes_task_definition && var.ignore_changes_load_balancer && var.ignore_changes_desired_count ? 1 : 0
   name                               = module.this.id
-  task_definition                    = coalesce(var.task_definition, "${join("", aws_ecs_task_definition.default.*.family)}:${join("", aws_ecs_task_definition.default.*.revision)}")
+  task_definition                    = local.create_task_definition ? "${join("", aws_ecs_task_definition.default[*].family)}:${join("", aws_ecs_task_definition.default[*].revision)}" : var.task_definition[0]
   desired_count                      = var.desired_count
   deployment_maximum_percent         = var.deployment_maximum_percent
   deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent
