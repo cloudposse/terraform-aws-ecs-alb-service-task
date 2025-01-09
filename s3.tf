5f01e585d4091bcd67c3091f0ff5a66aa1ed8311
Monica Hart <monica.hart@kin.com>
2025-01-09 15:58:34 -0600

    Adding appspec for CodeDeploy and S3 (#19)
    * adding appspec and s3
    
    * adding count index
    
    * adding missing content
    
    * enabling bucket versioning
    
    * switching to just content
    
    * updating the rule id
    
    * removing timestamp in appspec
    
    * updating terraform version
    
    * cleaning up the repo
    
    ---------
    
    Co-authored-by: Monica Hart <monica.hart@kin-mb-6846.lan>

---
+++ s3.tf
@@ -0,0 +1,61 @@
+## S3 Bucket for applications with CodeDeploy enabled only
+
+resource "aws_s3_bucket" "appspec_artifacts" {
+  count = var.deployment_controller_type == "CODE_DEPLOY" ? 1 : 0
+
+  bucket = "codedeploy-${local.container_name}-appspec"
+
+  tags = module.this.tags
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  count = var.deployment_controller_type == "CODE_DEPLOY" ? 1 : 0
+
+  bucket = aws_s3_bucket.appspec_artifacts[0].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "appspec_artifacts" {
+  count = var.deployment_controller_type == "CODE_DEPLOY" ? 1 : 0
+
+  bucket  = aws_s3_bucket.appspec_artifacts[0].id
+  key     = "${local.container_name}-appspec"
+  content = local.appspec_content
+  # Using etag for versioning, it will change if content changes
+  etag = local.appspec_sha256
+
+  tags = module.this.tags
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "appspec_artifacts" {
+  count = var.deployment_controller_type == "CODE_DEPLOY" ? 1 : 0
+
+  bucket = aws_s3_bucket.appspec_artifacts[0].id
+
+  rule {
+    id     = "store-old-versions"
+    status = "Enabled"
+
+    transition {
+      days          = 60
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 90
+      storage_class   = "GLACIER"
+    }
+  }
+}
